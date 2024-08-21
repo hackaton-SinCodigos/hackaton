36689f21d793c0d46c9285f1cbdb3ccec3f7aafa
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-08-21 15:16:14 -0300

    Environment variables are added for the connection to the database

--- server/config/environments.js
+++ server/config/environments.js
@@ -1,5 +1,13 @@
 import 'dotenv/config'
 
 export const environments = {
-  PORT: process.env.PORT
+  PORT: process.env.PORT,
+  DB: {
+    DATABASE_NAME: process.env.DB_NAME,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    HOST: process.env.DB_HOST,
+    DIALECT: process.env.DB_DIALECT,
+    PORT: process.env.DB_PORT,
+  }
 }
