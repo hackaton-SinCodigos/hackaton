6424bd7a407b95f52dac8b7d0972aa35f551af6a
Natanael Obed <zenagsuma5@gmail.com>
2024-08-22 10:16:39 -0300

    se añadieron controles y varios controladores

---
+++ server/controllers/cont.validations.js
@@ -0,0 +1,26 @@
+import { body } from "express-validator";
+
+//Validacion para el POST
+export const createMovieValidation = [
+    body("title")
+    .isString({
+        min: 5,
+        max: 155
+    })
+    .withMessage("El titulo solo debe contener letras.")
+    .notEmpty()
+    .trim
+    .withMessage("El titulo no puede estar vacio"),
+    body("clasification")
+    .notEmpty()
+    .withMessage("La clasificacion no puede estar vacia.")
+    .trim
+    .isIn(['G', 'PG', 'PG-13', 'R', 'NC-17'])
+    .withMessage("Clasificacion invalida"),
+    body("synopsis")
+    .isString({
+        min: 5,
+        max: 155
+    })
+    
+]
