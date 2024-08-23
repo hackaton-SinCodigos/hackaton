b1cec0979cb659b34de98114c70fa94802457bc7
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-08-23 15:16:59 -0300

    renderizado de peliculas

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -1,8 +1,19 @@
-import MovieCard from './MovieCard';
 import Sidebar from './SideBar';
 import styles from '../assets/css/movieList.module.css';
+import { useEffect, useState } from 'react';
+import { CustomFetch } from '../api/CustomFetch';
 
 const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    CustomFetch('http://localhost:4000/api/movie', 'GET')
+      .then((response) => setMovies(response.movies))
+      .catch((error) => console.error('Error:', error));
+  },[]);
+
+  console.log(movies);
+
   return (
     <div className={styles.movieList}>
       <Sidebar />
@@ -14,7 +25,18 @@ const MovieList = () => {
           </p>
         </div>
         <div className={styles.cardsContainer}>
-          <MovieCard />
+        {movies.map((movie, index) => (
+            <div key={index} className='card' style={{ width: '18rem' }}>
+              <img src={movie.portrait} className='card-img-top' alt={movie.title} />
+              <div className='card-body'>
+                <h5 className='card-title'>{movie.title}</h5>
+                <p className='card-text'>{movie.synopsis}</p>
+                <a href={`/movie/${movie.id}`} className='btn btn-primary'>
+                  Ver detalles
+                </a>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
