38cd226d596822055c6932ad0599df9cce5b5430
unknown <zenagsuma5@gmail.com>
2024-08-23 10:41:51 -0300

    se añadieron las validaciones finales

--- server/models/schemas/movie.shemas.js
+++ server/models/schemas/movie.shemas.js
@@ -1,63 +1,62 @@
 import { body } from "express-validator";
 
 export const createMovieSchema = [
-    body("title")
+  body("title")
     .isString({
-        min: 5,
-        max: 25
+      min: 5,
+      max: 25,
     })
     .withMessage("El titulo solo debe contener letras.")
     .notEmpty()
     .trim()
     .withMessage("El titulo no puede estar vacio"),
-    body("clasification")
+  body("clasification")
     .notEmpty()
     .withMessage("La clasificacion no puede estar vacia.")
     .trim()
-    .isIn(['G', 'PG', 'PG-13', 'R', 'NC-17'])
+    .isIn(["G", "PG", "PG-13", "R", "NC-17"])
     .withMessage("Clasificacion invalida"),
-    body("synopsis")
+  body("synopsis")
     .isString({
-        min: 5,
-        max: 100
+      min: 5,
+      max: 100,
     })
     .notEmpty()
     .withMessage("La sinopsis no puede estar vacia")
     .trim(),
-    body("portrait")
+  body("portrait")
     .notEmpty()
     .withMessage("La imagen de portada no puede estar vacia")
     .trim(),
-    body("trailer")
+  body("trailer")
     .notEmpty()
     .isString({
-        min: 10,
-        max: 100
+      min: 10,
+      max: 100,
     })
     .withMessage("El enlace del trailer no puede estar vacio")
     .trim(),
-    body("distribution")
+  body("distribution")
     .notEmpty()
     .withMessage("La distribucion no puede estar vacia")
     .isString({
-        min: 5,
-        max: 100
+      min: 5,
+      max: 100,
     })
     .trim(),
-    body("duration")
+  body("duration")
     .notEmpty()
     .withMessage("La duracion no puede estar vacia")
     .isString({
-        min: 10,
-        max: 300
+      min: 10,
+      max: 300,
     }),
-    body("director")
+  body("director")
     .notEmpty()
     .withMessage("El director no puede estar vacio")
     .isString({
-        min: 5,
-        max: 100
+      min: 5,
+      max: 100,
     })
-    .trim()
-    
-]
+    .trim(),
+];
